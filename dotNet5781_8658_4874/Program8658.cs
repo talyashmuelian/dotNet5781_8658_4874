d56110c3732a0658dcdd41b87de775be3b1ae07a
talya <talya535@gmail.com>
2020-10-20 09:53:47 +0300

    intial code+ welcome8658

---
+++ dotNet5781_8658_4874/Program8658.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_5781_00_8658_4874
+{
+	partial class Program
+	{
+		static void Main(string[] args)
+		{
+			Welcome8658();
+			Welcome4874();
+			Console.ReadKey();
+		}
+
+		static partial void Welcome4874();
+		private static void Welcome8658()
+		{
+			Console.WriteLine("Enter your name: ");
+			string name = Console.ReadLine();
+			Console.WriteLine("{0}, welcome to my first console application", name);
+		}
+	}
+}
