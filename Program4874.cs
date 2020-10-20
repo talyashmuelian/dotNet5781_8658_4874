671a979b812179c50d94f8c72068b5f5c9621140
נריה <@MATYABRS>
2020-10-20 11:51:33 +0300

    Hello world

---
+++ Program4874.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_5781_00_8658_4874
+{
+	partial class Program
+	{
+
+		static partial void Welcome4874()
+
+		{
+			Console.WriteLine("I am also here");
+		}
+			
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dotNet5781_8658_4874
 neria and talya dot net
+
+Hello world
