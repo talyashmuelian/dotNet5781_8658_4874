7877c053cd9efada0aa5fd0ec2470b4141f11531
talya <talya535@gmail.com>
2020-10-27 11:16:45 +0300

    hi

---
+++ dotNet5781_01_8658_4874/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_8658_4874
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Date
+    {
+        public int day;
+        public int nomth;
+        public int year;
+
+    }
+    class Bus
+    {
+        private int numOfBus;
+        private
+    }
+}
